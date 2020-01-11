27537736038bfa08be41391b95775a5b3ce8946f
xirikm <xirikm@gmail.com>
2020-01-11 22:21:28 +0800

    add login status check and multi-account support

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -2,6 +2,7 @@ import os
 import requests
 import time
 import random
+import re
 
 
 def randomly_gen_uspace_url():
@@ -29,20 +30,47 @@ def login(username, password):
     return s
 
 
-def get_points(s):
-    url_list = randomly_gen_uspace_url()
-    for url in url_list:
-        try:
-            s.get(url)
-            print("用户空间链接：" + url + " 访问成功")
-            time.sleep(4)
-        except Exception as e:
-            print(e)
-        continue
+def check_login_status(s, number_c):
+    # 获取设置页面标题以测试是否登录成功
+    test_url = "https://www.hostloc.com/home.php?mod=spacecp"
+    res = s.get(test_url)
+    res.encoding = "utf-8"
+    test_title = re.findall("<title>.*?</title>", res.text)
+    if test_title[0] != "<title>个人资料 -  全球主机交流论坛 -  Powered by Discuz!</title>":
+        print("第" + str(number_c) + "个账户登录失败！")
+        return False
+    else:
+        print("第" + str(number_c) + "个账户登录成功！")
+        return True
+
+
+def get_points(s, number_c):
+    if check_login_status(s, number_c):
+        url_list = randomly_gen_uspace_url()
+        for url in url_list:
+            try:
+                s.get(url)
+                print("用户空间链接：" + url + " 访问成功")
+                time.sleep(4)
+            except Exception as e:
+                print(str(e))
+            continue
+    else:
+        print("请检查你的帐户是否正确！")
 
 
 if __name__ == "__main__":
     username = os.environ["HOSTLOC_USERNAME"]
     password = os.environ["HOSTLOC_PASSWORD"]
-    s = login(username, password)
-    get_points(s)
+
+    user_list = username.split(",")
+    passwd_list = password.split(",")
+
+    for i in range(len(user_list)):
+        try:
+            s = login(user_list[i], passwd_list[i])
+            get_points(s, i + 1)
+            print("******************************")
+        except Exception as e:
+            print("密码个数不匹配：" + str(e))
+        continue
