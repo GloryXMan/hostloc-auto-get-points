9d3085bf501090aee893bd369cdbae7799680cea
xirikm <xirikm@gmail.com>
2020-01-11 16:35:59 +0800

    update python scripts

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -7,18 +7,17 @@ import random
 def randomly_gen_uspace_url():
     url_list = []
     # 生成的随机数可能会重复，懒得去重了，多生成几个就行了
-    for i in range(15):
+    for i in range(2):
         uid = random.randint(10000, 35000)
         url = "https://www.hostloc.com/space-uid-{}.html".format(str(uid))
         url_list.append(url)
     return url_list
 
 
-def get_points(username, password):
+def login(username, password):
     headers = {
         "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
     }
-    url_list = randomly_gen_uspace_url()
     login_url = "https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
     login_data = {
         "fastloginfield": "username",
@@ -27,11 +26,16 @@ def get_points(username, password):
     }
     s = requests.Session()
     s.post(url=login_url, data=login_data, headers=headers)
+    return s
+
+
+def get_points(s):
+    url_list = randomly_gen_uspace_url()
     for url in url_list:
         try:
             s.get(url)
             print("用户空间链接：" + url + " 访问成功")
-            time.sleep(2)
+            time.sleep(4)
         except Exception as e:
             print(e)
         continue
@@ -40,4 +44,5 @@ def get_points(username, password):
 if __name__ == "__main__":
     username = os.environ["HOSTLOC_USERNAME"]
     password = os.environ["HOSTLOC_PASSWORD"]
-    get_points(username, password)
+    s = login(username, password)
+    get_points(s)
