d6e51b8483cc8af9d3e6627c2f4401a26e1c3cf2
xirikm <xirikm@gmail.com>
2020-03-28 21:48:54 +0800

    fix typo

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -19,7 +19,7 @@ def randomly_gen_uspace_url():
 # 登录帐户
 def login(username, password):
     headers = {
-        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
     }
     login_url = "https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1"
     login_data = {
